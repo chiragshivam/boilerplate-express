a1d4d347337b93deac68a8ba4699c18812f2f1f4
chiragshivam <>
2021-10-05 16:05:57 +0000

    Adds modification

--- myApp.js
+++ myApp.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var app = express();
 
+console.log(__dirname);
+var absolutePath = __dirname + "/views/index.html";
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(absolutePath);
+});
 
-
-
-
-
-
-
-
-
+app.get("/json", (req, res) => {
+  res.json({"message": "Hello json"});
+});
 
 
 
