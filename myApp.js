3f5dd97972a93043c3fa1ef656de062a2a0b008b
Chirag Chopra <chiragshivamccsc@gmail.com>
2021-10-05 21:39:24 +0530

    Adds env

--- myApp.js
+++ myApp.js
@@ -10,7 +10,10 @@ app.get("/", (req, res) => {
 });
 
 app.get("/json", (req, res) => {
-  res.json({"message": "Hello json"});
+    if(process.env.MESSAGE_STYLE == "uppercase")
+        res.json({"message": "HELLO JSON"});
+    else
+    res.json({"message": "Hello json"});
 });
 
 
